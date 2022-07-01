675ab3d79e899df63d2a2e0e236a45df6b47cd75
Marcio Lima <ma4cio@gmail.com>
2022-07-01 08:56:32 -0300

    Login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
@@ -10,13 +11,16 @@ export class AuthService {
   private url: string = 'http://localhost:3000';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
   
   public sign(payLoad: {email: string, password: string}): Observable<any>{
-    return this.http.post(`${this.url}/sign`, payLoad).pipe(
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payLoad).pipe(
       map((res)=> {
-        return console.log(res);
+        localStorage.removeItem('access_token')
+        localStorage.setItem('access_token', res.token)
+        return this.router.navigate(['admin']);
       }),
       catchError((err) => {
         console.error(err);
@@ -27,4 +31,8 @@ export class AuthService {
     )
   }
 
+  public logout(){
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
+  }
 }
